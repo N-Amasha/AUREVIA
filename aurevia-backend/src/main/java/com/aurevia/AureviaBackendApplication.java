81a3aa299aa887b4493ff7962c92b48212cca705
Nethmi Amasha <ne.amasha@hotmail.com>
2026-09-25 17:15:25 +0530

    feat(backend): complete Spring Boot foundation

---
+++ aurevia-backend/src/main/java/com/aurevia/AureviaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aurevia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AureviaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AureviaBackendApplication.class, args);
+	}
+
+}
---
+++ aurevia-backend/src/test/java/com/aurevia/AureviaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aurevia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AureviaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
